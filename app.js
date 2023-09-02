8683e8a9c1a45052bd1f36e2a181b28dbab02f58
toduyemi <temit96@hotmail.com>
2023-09-02 14:11:56 -0400

    Sort out functionality that adds data attribute

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const newBookModal = document.querySelector('#newBookModal');
 const addBookForm = document.querySelector('#addBookForm')
 const shelveButton = document.querySelector('#shelveBook');
 const closeDialogButton = document.querySelector('#closeDialog');
+let removeBookButtons;
 let NewBook;
 
 function addBookToNook(book) {
@@ -16,31 +17,52 @@ function Book(title, author, pages, status) {
     this.author = author;
     this.pages = pages;
     this.status = status;
+
+    // prevent reprint upon new books
     this.printed = false;
 }
 
-const displayNook = () => {
+Book.prototype.getDataIndex = function() {
+    let index = myNook.findIndex(book => {
+        console.log(book.title);
+        console.log(this.title);
+        return book.title === this.title;
+    });
+    console.log(index);
+    return index;
+}
+
+function addDeleteButtonToArray() {
+    removeBookButtons = document.querySelectorAll('.removeBook');
+}
+
+function displayNook() {
     console.log(myNook);
     myNook.forEach((book) => {
         if (!book.printed) {
             const bookCard = document.createElement('div');
-
+            bookCard.setAttribute('data-index', book.getDataIndex());
 
             const bookTitle = document.createElement('div');
             const bookAuthor = document.createElement('div');
             const bookPages = document.createElement('div');
             const bookRead = document.createElement('div');
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('removeBook');
+            addDeleteButtonToArray();
 
             bookTitle.textContent = `Title: ${book.title}`;
             bookAuthor.textContent = `Author: ${book.author}`;
             bookPages.textContent = `Pages: ${book.pages}`;
             bookRead.textContent = `Status: ${book.status}`;
+            deleteButton.textContent = 'remove';
 
 
             bookCard.appendChild(bookTitle);
             bookCard.appendChild(bookAuthor);
             bookCard.appendChild(bookPages);
             bookCard.appendChild(bookRead);
+            bookCard.appendChild(deleteButton);
 
             nook.appendChild(bookCard);
             book.printed = true;
@@ -49,6 +71,7 @@ const displayNook = () => {
     });
 
 }
+
 
 
 newBookButton.addEventListener('click', () => {
@@ -71,4 +94,4 @@ addBookForm.addEventListener('submit', (e) => {
     displayNook();
 });
 
-
+removeBookButtons.forEach
