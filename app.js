4d60b8cf1703aaad3c14e0ef9920bbf3027db500
toduyemi <temit96@hotmail.com>
2023-09-10 18:38:14 -0400

    Fix edit bug where eventListeners were stackign for every edit. Revert "Tried to refactor to utilize one form didnt work. will revert"
    This reverts commit f726a7c55097add0b6c37fb3dc8b3a85984ff54c.
    
    Nested editForm submit event listener in the edit button. Also
    added function to remove that event listener in the call back function.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const myNook = [];
 const nook = document.querySelector('#nook');
 const newBookButton = document.querySelector(' #newBookButton');
 const newBookModal = document.querySelector('#newBookModal');
+const editBookModal = document.querySelector('#editBookModal');
 const addBookForm = document.querySelector('#addBookForm')
 const shelveButton = document.querySelector('#shelveBook');
 const closeDialogButton = document.querySelector('#closeDialog');
-// const closeEditDialogButton = document.querySelector('#editBookForm #closeDialog');
+const closeEditDialogButton = document.querySelector('#editBookForm #closeDialog');
 let NewBook;
 
 
@@ -19,33 +20,15 @@ function Book(title, author, pages, progress) {
     // prevent reprint upon new books
     this.printed = false;
 
-    //made editBookModal element a property of card so it would have access to element properties
-    //does this really need to be a property of constructor?
-    this.editBookModal = document.querySelector('#newBookModal');
-    this.editBookForm = document.querySelector('#addBookForm');
-
-
-    // this.editBookForm.prototype.prefillEditBookForm = function () {
-    //     this
-    // }
-
-}
-
-Book.prototype.createBookCard = function () {
-    //create bookcard div as a property of Book constructor
-    this.bookCard = document.createElement('div');
-    this.bookCard.classList.add('bookCard');
-
-
-    //also create delete and edit buttons to be properties of Book constructor
-    //this is done to intrinsically bind each element to its parent object
-    bookTitle = document.createElement('div');
-    bookAuthor = document.createElement('div');
-    bookPages = document.createElement('div');
-    bookRead = document.createElement('div');
+    this.editBookForm = document.querySelector('#editBookForm');
     this.deleteButton = document.createElement('button');
     this.editButton = document.createElement('button');
+    this.deleteButton.textContent = 'remove';
+    this.editButton.textContent = 'edit';
 
+
+    //implement delete and edit buttons to be properties of Book constructor
+    //this is done to intrinsically bind each element to its parent object
     this.deleteButton.classList.add('bookCardButton');
     this.deleteButton.addEventListener('click', function () {
         let bookIndex = this.getAttribute('data-index');
@@ -54,61 +37,87 @@ Book.prototype.createBookCard = function () {
 
     this.editButton.classList.add('bookCardButton');
     this.editButton.addEventListener('click', () => {
-        this.editBookModal.showModal();
+
+        // event Handler tracker prevents an eventhandler 
+        // from being added to form every time the edit button is clicked
+        this.eventHandlerTracker;
+        editBookModal.showModal();
         this.prefillEditBookForm();
+
+        this.updateEditBook = this.updateEditBook.bind(this);
+        this.editBookForm.addEventListener('submit', this.updateEditBook);
 
     });
 
-    bookTitle.textContent = `Title: ${this.title}`;
-    bookAuthor.textContent = `Author: ${this.author}`;
-    bookPages.textContent = `Pages: ${this.pages}`;
-    bookRead.textContent = `Status: ${this.progress}`;
-    this.deleteButton.textContent = 'remove';
-    this.editButton.textContent = 'edit';
 
-    this.bookCard.appendChild(bookTitle);
-    this.bookCard.appendChild(bookAuthor);
-    this.bookCard.appendChild(bookPages);
-    this.bookCard.appendChild(bookRead);
+}
+
+//method to create bookCard html element
+Book.prototype.createBookCard = function () {
+    //create bookcard div as a property of Book constructor
+    this.bookCard = document.createElement('div');
+    this.bookCard.classList.add('bookCard');
+
+    //these properties represent html elements bound to each book object constructed
+    this.bookTitle = document.createElement('div');
+    this.bookAuthor = document.createElement('div');
+    this.bookPages = document.createElement('div');
+    this.bookRead = document.createElement('div');
+
+    this.bookTitle.textContent = `title: ${this.title}`;
+    this.bookAuthor.textContent = `author: ${this.author}`;
+    this.bookPages.textContent = `pages: ${this.pages}`;
+    this.bookRead.textContent = `status: ${this.progress}`;
+
+    this.bookCard.appendChild(this.bookTitle);
+    this.bookCard.appendChild(this.bookAuthor);
+    this.bookCard.appendChild(this.bookPages);
+    this.bookCard.appendChild(this.bookRead);
     this.bookCard.appendChild(this.deleteButton);
     this.bookCard.appendChild(this.editButton);
 
     return this.bookCard;
 }
 
+//method to set an id from 
 Book.prototype.setDataIndex = function () {
-    const bookCard = this.bookCard;
-    let index = myNook.indexOf(this);
+    this.id = myNook.indexOf(this);
 
-    this.id = index;
-
-    bookCard.setAttribute('data-index', index);
+    this.bookCard.setAttribute('data-index', this.id);
 
     //declared and attribute set down here due to dependency of bookCard dataindex css locator
-    const bookCardButtons = document.querySelectorAll(`.bookCard[data-index="${index}"] .bookCardButton`);
-    bookCardButtons.forEach((card) => card.setAttribute('data-index', index));
+    const bookCardButtons = document.querySelectorAll(`.bookCard[data-index="${this.id}"] .bookCardButton`);
+    bookCardButtons.forEach((card) => card.setAttribute('data-index', this.id));
 
-
-    return index;
+    return this.id;
 }
 
-Book.prototype.editBook = function () {
+Book.prototype.updateEditBook = function () {
+    this.title = this.editBookForm.bookTitle.value;
+    this.author = this.editBookForm.bookAuthor.value;
+    this.pages = this.editBookForm.bookPages.value;
+    this.progress = this.editBookForm.bookRead.value;
 
+    this.bookTitle.textContent = `title: ${this.title}`;
+    this.bookAuthor.textContent = `author: ${this.author}`;
+    this.bookPages.textContent = `pages: ${this.pages}`;
+    this.bookRead.textContent = `status: ${this.progress}`;
+
+    // prevents event listeners from stacking => future calls will change any card already editted
+    this.editBookForm.removeEventListener('submit', this.updateEditBook);
 }
 
+//method to placehold values from relevant bookCard to edit form modal
 Book.prototype.prefillEditBookForm = function () {
     this.editBookForm.bookTitle.value = this.title;
     this.editBookForm.bookAuthor.value = this.author;
     this.editBookForm.bookPages.value = this.pages;
-    document.querySelector(`[value="${this.progress}"]`).checked = true;
-
-
+    document.querySelector(`#editBookForm [value="${this.progress}"]`).checked = true;
 }
 
 
 function resetAllIds() {
     myNook.forEach(book => book.setDataIndex());
-    this;
 }
 
 function addBookToNook(book) {
@@ -129,7 +138,7 @@ function displayNook() {
     myNook.forEach((book) => {
         if (!book.printed) {
             bookCard = book.createBookCard();
-            console.log(bookCard);
+            // console.log(bookCard);
             nook.appendChild(bookCard);
 
             //so book doesn't display again
@@ -138,7 +147,20 @@ function displayNook() {
     });
 }
 
+//prefill 
+RedRising = new Book('Red Rising', 'Pierce Brown', 600, 'read');
+addBookToNook(RedRising);
+BewareofChicken = new Book('Beware of Chicken Vol 1', 'Casualfarmer', 500, 'read');
+addBookToNook(BewareofChicken);
+displayNook();
 
+addBookForm.addEventListener('submit', function () {
+    NewBook = new Book(this.bookTitle.value, this.bookAuthor.value, this.bookPages.value, this.bookRead.value);
+    // console.log(NewBook);
+    addBookToNook(NewBook);
+    displayNook();
+    NewBook.setDataIndex();
+});
 
 newBookButton.addEventListener('click', () => {
     newBookModal.showModal();
@@ -146,20 +168,8 @@ newBookButton.addEventListener('click', () => {
 
 closeDialogButton.addEventListener('click', () => {
     newBookModal.close();
-})
-
-addBookForm.addEventListener('submit', function (e) {
-    if (this === addBookForm) {
-        console.log(this)
-        NewBook = new Book(this.bookTitle.value, this.bookAuthor.value, this.bookPages.value, this.bookRead.value);
-        console.log(NewBook);
-        addBookToNook(NewBook);
-        displayNook();
-        NewBook.setDataIndex();
-    }
-
-    // else if (this === Book.prototype)
-
 });
 
-
+closeEditDialogButton.addEventListener('click', () => {
+    editBookModal.close();
+});
